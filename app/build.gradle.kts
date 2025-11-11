ced0a1bf55de4a97e349a61945a92f87d399a8fe
Lasercl <laserclauss2612@gmail.com>
2025-11-11 23:48:02 +0700

    add obsification Obfuscation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,11 +24,15 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
